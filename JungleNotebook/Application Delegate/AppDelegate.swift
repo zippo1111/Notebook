fd5c901fd1f032cadab9186bcb1d083011f8939d
buddybuild <help@buddybuild.com>
2017-12-23 16:08:12 -0800

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- JungleNotebook/Application Delegate/AppDelegate.swift
+++ JungleNotebook/Application Delegate/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Configure Window
         window?.tintColor = UIColor(red:0.99, green:0.47, blue:0.44, alpha:1.0)
 
